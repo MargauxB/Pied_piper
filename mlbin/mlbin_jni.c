c08be36db9575411516821b7c6d3937bbf39e79b
Margaux <margaux.barillot@gmail.com>
2016-12-02 16:00:13 +0100

    coucou la famille on a modifié mlbin encore une fois et fait le JNI

--- mlbin/mlbin_jni.c
+++ mlbin/mlbin_jni.c
@@ -2,15 +2,15 @@ mlb_init_usb JNIEXPORT jint JNICALL Java_com.android.PiedPiper_InitUsb mlb_init_
 
 mlbin_free_usb JNIEXPORT jint JNICALL Java_com.android.PiedPiper_InitUsb mlb_free_usb(JNIEnv*env, Jobject this)
 
-mlbin_fire JNIEXPORT jint JNICALL Java_com.android.PiedPiper_InitUsb mlbin_fire(JNIEnv*env, Jobject this, Jint device)
+mlbin_fire JNIEXPORT jint JNICALL Java_com.android.PiedPiper_InitUsb mlbin_fire(JNIEnv*env, Jobject this)
 
-mlbin_move_down JNIEXPORT jint JNICALL Java_com.android.PiedPiper_InitUsb mlbin_move_down(JNIEnv*env, Jobject this, Jint device)
+mlbin_move_down JNIEXPORT jint JNICALL Java_com.android.PiedPiper_InitUsb mlbin_move_down(JNIEnv*env, Jobject this
 
-mlbin_move_left JNIEXPORT jint JNICALL Java_com.android.PiedPiper_InitUsb mlbin_move_left(JNIEnv*env, Jobject this, Jint device)
+mlbin_move_left JNIEXPORT jint JNICALL Java_com.android.PiedPiper_InitUsb mlbin_move_left(JNIEnv*env, Jobject this)
 
-mlbin_move_right JNIEXPORT jint JNICALL Java_com.android.PiedPiper_InitUsb mlbin_move_right(JNIEnv*env, Jobject this, Jint device)
+mlbin_move_right JNIEXPORT jint JNICALL Java_com.android.PiedPiper_InitUsb mlbin_move_right(JNIEnv*env, Jobject this)
 
-mlbin_move_up JNIEXPORT jint JNICALL Java_com.android.PiedPiper_InitUsb mlbin_move_up(JNIEnv*env, Jobject this, Jint device)
+mlbin_move_up JNIEXPORT jint JNICALL Java_com.android.PiedPiper_InitUsb mlbin_move_up(JNIEnv*env, Jobject this)
 
 mlbin_stop JNIEXPORT jint JNICALL Java_com.android.PiedPiper_InitUsb mlb_stop(JNIEnv*env, Jobject this)
 
