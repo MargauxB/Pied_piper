50f72716dee45dec57841091d2dc83c295c65b6f
lo52tp <lo52tp@lo52tp-VirtualBox.(none)>
2016-12-16 13:21:18 +0100

    Bonjour

---
+++ mlbin/java/com/android/PiedPiper/InitUsb/main.java
@@ -0,0 +1,13 @@
+public static void main(String [ ] args){
+	thows InterruptedException{	
+		InitUsb BoumBoum = new InitUsb();
+		BoumBoum.mlbInitUsb();
+		Thread.sleep(1000);
+		BoumBoum.mlbinFire();
+		Thread.sleep(2000);
+		BoumBoum.mlbinMoveDown();
+		Thread.sleep(2000);
+		BoumBoum.mlbinMoveLeft();
+		Thread.sleep(2000);
+		BoumBoum.mlbinMoveRight();
+}
